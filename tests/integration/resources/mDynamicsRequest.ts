6f24268c5fb42996298a0c0387bc22c46371255a
Aaron Downing <aaron.downing@bjss.com>
2022-08-19 10:48:00 +0100

    feat(cb2-5236): handle array of line items

--- tests/integration/resources/mDynamicsRequest.ts
+++ tests/integration/resources/mDynamicsRequest.ts
@@ -1,3 +1,3 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 
-export const mDynamicsRequest: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ body: '[{"name":"hello","bookingDate":"2022-01-01", "vrm":"12345","testCode":"12345","testDate":"2022-01-01","pNumber":"12345"}]' };
+export const mDynamicsRequest: APIGatewayProxyEvent = <APIGatewayProxyEvent>{ body: JSON.stringify([{ name:'hello', bookingDate: '2022-01-01', vrm:'12345', testCode:'12345', testDate:'2022-01-01', pNumber:'12345' }]) };
